f177de08d87bfcf8f2d18f898f81c3772ef638a7
Robert Wu <wu.shawn799@gmail.com>
2024-11-03 23:27:18 +0800

    新增 查詢所有資料api

--- routes/products.js
+++ routes/products.js
@@ -3,8 +3,11 @@ import db from "../utils/connect-sql.js";
 
 const router = express.Router();
 
+// 取得所有產品資料
 router.get("/", async (req, res) => {
-  const sql = "SELECT * FROM product_list";
+  const sql =
+    "SELECT p.product_id id, p.title, c.product_category_name category, p.description, p.price, p.img_url image FROM product_list p JOIN product_categories c ON p.product_category_id = c.product_category_id;";
+
   const [rows] = await db.query(sql);
   res.json(rows);
 });
