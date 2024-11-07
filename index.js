e013f62e6545fc891b33277dc008af6375bdd8dd
Shirley <shirley109000@gmail.com>
2024-11-07 12:27:48 +0800

    提交一下

--- index.js
+++ index.js
@@ -45,14 +45,12 @@ app.use("/divesite", divesite);
 //app.use("/lesson", lesson);
 //app.use("/member", member);
 app.use("/api/blog", blog);
-<<<<<<< HEAD
 app.use("/api/message", message);
 app.use("/auth", authRoutes);
 app.use("/profile", memberProfile);
-=======
 app.use("/api/comment", comment);
 
->>>>>>> fanny
+
 
 // 測試路由
 app.get("/test", async (req, res) => {
